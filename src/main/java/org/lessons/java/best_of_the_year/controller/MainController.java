52a8d931d1a3fe9e8d9f4143eacdef509bd9cc3f
MarcoVacchi <mvacchi816@gmail.com>
2025-07-22 21:38:53 +0200

    feat: added list

--- src/main/java/org/lessons/java/best_of_the_year/controller/MainController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controller/MainController.java
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/")
 
 public class MainController {
-    
-    private ArrayList<Movies> moviesList = new ArrayList<>();
-    private ArrayList<Songs> songsList= new ArrayList<>();
 
-    public MainController(){
+    private ArrayList<Movies> moviesList = new ArrayList<>();
+    private ArrayList<Songs> songsList = new ArrayList<>();
+
+    public MainController() {
 
         Songs song = new Songs("1", "Somebody that i used to know");
         Songs song1 = new Songs("2", "Rap god");
@@ -26,7 +26,7 @@ public class MainController {
         songsList.add(song);
         songsList.add(song1);
         songsList.add(song2);
-    
+
         Movies movie = new Movies("1", "Titanic");
         Movies movie1 = new Movies("2", "La bella e la bestia");
         Movies movie2 = new Movies("3", "Fuga da alkatraz");
@@ -35,82 +35,38 @@ public class MainController {
         moviesList.add(movie2);
     };
 
-    private String getBestSongs(){
-    String title = "";
-    for (Songs songs : songsList) {
-    title += songs.getTitle()+", ";    
-        } return title;
+    @GetMapping("/movies")
+    private String getBestMovies(Model model) {
+        model.addAttribute("movies", moviesList);
+        return "movies";
     }
 
-    private String getBestMovies(){
-    String title = "";
-    for (Movies movies : moviesList) {
-    title += movies.getTitle()+", ";    
-        } return title;
+    @GetMapping("/songs")
+    private String getBestSongs(Model model) {
+        model.addAttribute("songs", songsList);
+        return "songs";
     }
-
-    //STEP 4
-    // private ArrayList<Movies> getBestMovies(){
-    //     final ArrayList<Movies> movieList = new ArrayList<>();
-    //     Movies movie = new Movies("1", "Titanic");
-    //     Movies movie1 = new Movies("2", "La bella e la bestia");
-    //     Movies movie2 = new Movies("3", "Fuga da alkatraz");
-    //     movieList.add(movie);
-    //     movieList.add(movie1);
-    //     movieList.add(movie2);
-    //     return movieList;
-    // }
-       // @GetMapping("/movies")
-    // public String getBestMovies(Model model) {
-    //     model.addAttribute("movies", getBestMovies());
-    //     return "movies";
-    // }
-
-    //   private String getBestSongs(){
-
-    //   private ArrayList<Songs> songList = new ArrayList<>();
-
-    //     Songs song = new Songs("1", "Somebody that i used to know");
-    //     Songs song1 = new Songs("2", "Rap god");
-    //     Songs song2 = new Songs("3", "All eyez on me");
-    //     songList.add(song);
-    //     songList.add(song1);
-    //     songList.add(song2);
-    //     return songList.toString();
-    // }
 
     @GetMapping("/movies/{id}")
-    private String getTitleMovie(Model model, @PathVariable ("id") String idMovie){
-        for (Movies movie : moviesList) {   
+    private String getTitleMovie(Model model, @PathVariable("id") String idMovie) {
+        for (Movies movie : moviesList) {
             if (movie.getId().equalsIgnoreCase(idMovie)) {
                 model.addAttribute("movie", movie.getTitle());
                 return "movieDetail";
             }
         }
-        return "error"; 
-    }
-
-    @GetMapping("/songs/{id}")
-    private String getTitleSong(Model model, @PathVariable ("id") String idSong){
-        for (Songs song: songsList ){
-            if (song.getId().equalsIgnoreCase(idSong)) {
-                model.addAttribute("song", song.getTitle());
-                return "songDetail";    
-            }  
-        }
         return "error";
     }
 
-  @GetMapping("/movies")
-    private String getTitleMovies(Model model) {
-        model.addAttribute("title", getBestMovies());
-        return "movies";
-    }
-
-    @GetMapping("/songs")
-    private String getTitleSongs(Model model) {
-        model.addAttribute("title", getBestSongs());
-        return "movies";
+    @GetMapping("/songs/{id}")
+    private String getTitleSong(Model model, @PathVariable("id") String idSong) {
+        for (Songs song : songsList) {
+            if (song.getId().equalsIgnoreCase(idSong)) {
+                model.addAttribute("song", song.getTitle());
+                return "songDetail";
+            }
+        }
+        return "error";
     }
 
     @GetMapping("/")
@@ -119,5 +75,16 @@ public class MainController {
         model.addAttribute("name", "Marco");
         return "homepage";
     }
-
 }
+
+// Step 4
+// Includere Bootstrap e fare il refactoring del layout come da allegato,
+// cercando di creare componenti riutilizzabili con i fragments.
+
+// Modificare i metodi che rispondono agli url
+// “/movies”
+// “/songs”
+// In modo che entrambi i Model restituiscano una lista di oggetti (Movie o
+// Song) invece di una stringa.
+
+// Modificare anche le rispettive view.
